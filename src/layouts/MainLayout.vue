26c824235f292f6bcb51848a04335436e4bbe50c
ILYA YAROSH <ilya.n.yarosh@gmail.com>
2021-12-09 21:26:49 +0300

    added base templates

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="app-main-layout">
+    <nav class="navbar purple lighten-3">
+      <div class="nav-wrapper">
+        <div class="navbar-left">
+          <a href="#">
+            <i class="material-icons black-text">dehaze</i>
+          </a>
+          <span class="black-text">12.12.12</span>
+        </div>
+
+        <ul class="right hide-on-small-and-down">
+          <li>
+            <a
+              class="dropdown-trigger black-text"
+              href="#"
+              data-target="dropdown"
+            >
+              USER NAME
+              <i class="material-icons right">arrow_drop_down</i>
+            </a>
+
+            <ul id="dropdown" class="dropdown-content">
+              <li>
+                <a href="#" class="black-text">
+                  <i class="material-icons">account_circle</i>Профиль
+                </a>
+              </li>
+              <li class="divider" tabindex="-1"></li>
+              <li>
+                <a href="#" class="black-text">
+                  <i class="material-icons">assignment_return</i>Выйти
+                </a>
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+    </nav>
+
+    <ul class="sidenav app-sidenav open">
+      <li>
+        <a href="#" class="waves-effect waves-grey pointer">Редактор сеансов</a>
+      </li>
+    </ul>
+
+    <main class="app-content">
+      <div class="app-page">
+        <router-view />
+      </div>
+    </main>
+
+    <div class="fixed-action-btn">
+      <a class="btn-floating btn-large blue" href="#">
+        <i class="large material-icons">add</i>
+      </a>
+    </div>
+  </div>
+</template>
--- src/router/index.js
+++ src/router/index.js
@@ -8,13 +8,36 @@ const routes = [
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/login',
+    name: 'login',
+    meta: { layout: 'empty' },
+    component: () => import('../views/Login.vue')
+  },
+  {
+    path: '/registration',
+    name: 'registration',
+    meta: { layout: 'empty' },
+    component: () => import('../views/Registration.vue')
+  },
+  {
+    path: '/sessions',
+    name: 'sessions',
+    meta: { layout: 'main' },
+    component: () => import('../views/Sessions.vue')
+  },
+  {
+    path: '/profile',
+    name: 'profile',
+    meta: { layout: 'main' },
+    component: () => import('../views/Profile.vue')
+  },
+  {
+    path: '/main',
+    name: 'main',
+    meta: { layout: 'main' },
+    component: () => import('../views/MainPage.vue')
   }
+
 ]
 
 const router = createRouter({
