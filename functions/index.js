ed7b36450126e8210b4ada58af3cc9d8c6cf536a
ILYA YAROSH <ilya.n.yarosh@gmail.com>
2022-01-26 00:03:27 +0300

    added functions

--- functions/index.js
+++ functions/index.js
@@ -1,24 +1,24 @@
-// const functions = require('firebase-functions')
-// const admin = require('firebase-admin')
+const functions = require('firebase-functions')
+const admin = require('firebase-admin')
 
-// admin.initializeApp()
+admin.initializeApp()
 
-// const db = admin.firestore()
+const db = admin.firestore()
 
-// exports.AddUserRole = functions.auth.user().onCreate(async (authUser) => {
-//   if (authUser.email) {
-//     const customClaims = {
-//       admin: true
-//     }
-//     try {
-//       var _ = await admin.auth().setCustomUserClaims(authUser.uid, customClaims)
+exports.AddUserRole = functions.auth.user().onCreate(async (authUser) => {
+  if (authUser.email) {
+    const customClaims = {
+      admin: true
+    }
+    try {
+      await admin.auth().setCustomUserClaims(authUser.uid, customClaims)
 
-//       return db.collection('roles').doc(authUser.uid).set({
-//         email: authUser.email,
-//         role: customClaims
-//       })
-//     } catch (error) {
-//       console.log(error)
-//     }
-//   }
-// })
+      return db.collection('roles').doc(authUser.uid).set({
+        email: authUser.email,
+        role: customClaims
+      })
+    } catch (error) {
+      console.log(error)
+    }
+  }
+})
